ccbae6467f262a5605999ef20e2cdb7f6774013c
wendi023 <148845610+wendi023@users.noreply.github.com>
2023-11-07 14:41:28 +0700

    Add files via upload

---
+++ Latihan4a.php
@@ -0,0 +1,16 @@
+<?php
+$warna = ["hijau, ","kuning, ","kelabu, ","merah muda,"];
+echo "Balonku ada lima. </br>";
+echo "Rupa-rupa warna-nya </br>";
+print_r ($warna[0]);
+print_r ($warna[1]);
+print_r ($warna[2]);
+print_r ($warna[3]);
+echo "";
+$warna[4] = " biru";
+echo " dan";
+echo $warna[4];
+echo "</br>";
+echo "Meletus balon $warna[0] DOR!!! </br>";
+echo "Hatiku sangat kacau. </br>";
+?>
