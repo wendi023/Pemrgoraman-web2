e60c9cdacad72207eeb51a341041f2b2e36d26b0
wendi023 <148845610+wendi023@users.noreply.github.com>
2023-11-08 23:35:10 +0700

    Add files via upload

---
+++ NrpLatihan5.php
@@ -0,0 +1,157 @@
+<?php 
+$Pahlawan = array(
+    1=> array(
+        "Foto" => "<img src = 'Ahmad_Yani.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Jenderal (Anumerta) Ahmad Yani",
+        "Lahir" => "Purworejo, 19 Juni 1922",
+        "Karier" => "Ikut pendidikan Heiho di Magelang dan Pembela Tanah Air (PETA) di Bogor
+- Ikut serta dalam pemberantasan PKI Madiun 1948, Agresi Militer Belanda II, penumpasan DI/TII di Jawa Tengah, dan PRRI Semesta di Sumatera.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+    2=> array(
+        "Foto" => "<img src = 'Soeprapto.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Letnan Jenderal TNI (Anumerta) Raden Suprapto",
+        "Lahir" => "Purwokerto, 20 Juni 1920",
+        "Karier" => "Pernah menjabat sebagai Kepala Staf Tentara Teritorium IV Diponegoro Semarang.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+     3=> array(
+        "Foto" => "<img src = 'S_Parman.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Letnan Jenderal TNI (Anumerta) Siswondo Parman",
+        "Lahir" => "Wonosobo, 4 Agustus 1918",
+        "Karier" => "Fokus pada pendidikan di bidang intelijen hingga pernah dikirim ke Jepang untuk memperdalam ilmu intelijen di Kenpei Kasya 
+                - Selama pendudukan Jepang, bekerja pada Jawatan Kenpeitai (polisi militer)",
+        "Wafat" => "1 Oktober 1966",
+        ),
+     4=> array(
+        "Foto" => "<img src = 'MT_Haryono.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Letnan Jenderal TNI (Anumerta) Mas Tirtodarmo Haryono",
+        "Lahir" => "Surabaya, 20 Januari 1924",
+        "Karier" => "Sebelum terjun ke dunia militer, pernah mengikuti Ika Dai Gaku (sekolah kedokteran) di Jakarta pada masa pendudukan Jepang
+                - Setelah kemerdekaan Indonesia bergabung bersama TKR dengan pangkat mayor
+                - Karena kepandaiannya berbahasa Belanda, Inggris, dan Jerman, M.T. Haryono mengikuti beberapa perundingan antara Indonesia dan Belanda atau Indonesia dan Inggris
+                - Saat Konferensi Meja Bundar, ia menjadi Sekretaris Delegasi Militer Indonesia.
+                - Di kementerian Pertahanan
+                - Direktur Intendans dan Deputi Ill Menteri/Panglima Angkatan Darat pada tahun 1964.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+     5=> array(
+        "Foto" => "<img src = 'Panjaitan.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Mayor Jenderal TNI (Anumerta) Donald Isaac Pandjaitan",
+        "Lahir" => "Balige, 9 Juni 1925",
+        "Karier" => "- Pada masa pendudukan Jepang ia memasuki pendidikan militer Gyugun
+                - Ikut membentuk TKR sebagai Komandan Batalyon ketika Indonesia merdeka
+                - Pada Agresi militer Belanda II ia bertugas sebagai PImpinan Perbekalan perjuangan Pemerintahan Darurat Republik Indonesia (PDRI)
+                - Menjabat sebagai Asisten IV Menteri/Panglima Angkatan Darat dan mendapat tugas belajar ke Amerika Serikat",
+        "Wafat" => "1 Oktober 1965",
+        ),
+     6=> array(
+        "Foto" => "<img src = 'Sutoyo.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Mayor Jenderal TNI (Anumerta) Sutoyo Siswomiharjo",
+        "Lahir" => "Kebumen, 28 Agustus 1922",
+        "Karier" => "- Pada masa pendudukan Jepang mengikuti pendidikan di Balai Pendidikan Pegawai Tinggi Jakarta
+                - Menjadi pegawai negeri di Kantor Kabupaten di Purworejo
+                - Setelah Proklamasi Kemerdekaan masuk TKR di bagian Kepolisian
+                - Menjadi anggota Korps Polisi Militer
+                - Menjadi ajudan Kolonel Gatot Subroto
+                - Kepala Bagian Organisasi Resimen II Polisi Tentara di Purworejo.
+                - Sempat menjabat sebagai Kepala Staf Markas Besar Polisi Militer pada 1954
+                - Menjadi Asisten Atase Militer RI untuk Inggris pada 1956
+                - Pada tahun 1961 diberi tugas sebagai Inspektur Kehakiman/Oditur Jenderal Angkatan Darat.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+     7=> array(
+        "Foto" => "<img src = 'Tendean.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Kapten Czi. (Anumerta) Pierre Andreas Tendean",
+        "Lahir" => "Jakarta, 21 Februari 1939",
+        "Karier" => "- Mengikuti pendidikan Jurusan Teknik di Akademi Militer
+                - Pada tahun 1962 ia sebagai menjabat Komandan Peleton Batalyon Zeni Tempur 2 Komando Daerah Militer II/Bukit Barisan di Medan
+                - Ditugaskan untuk menyusup ke daerah Malaysia pada peristiwa konfrontasi Malaysia.
+                - Pada bulan April 1965, diangkat sebagai ajudan Menteri Koordinator Pertahanan Keamanan/ Kepala Staf Angkatan Bersenjata Jenderal Nasution.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+      8=> array(
+        "Foto" => "<img src = 'Sugiyono.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Kolonel (Anumerta) Sugiyono Mangunwiyoto",
+        "Lahir" => "Gunung Kidul, 12 Agustus 1926",
+        "Karier" => "- Pada masa pendudukan Jepang Sugiyono mendapat pendidikan militer pada Pembela Tanah Air (PETA)
+                - Diangkat menjadi Budanco di Wonosari
+                - Mengikuti beberapa penumpasan pemberontakan di Tanah Air
+                - Pada masa kemerdekaan, Sugiyono masuk TKR Yogyakarta sebagai Komandan Seksi
+                - Pada 1947 ia diangkat sebagai Komandan Brigade 10 di bawah pimpinan Letnan Kolonel Soeharto.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+      9=> array(
+        "Foto" => "<img src = 'Katamso.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Brigadir Jenderal TNI (Anumerta) Katamso Darmokusumo",
+        "Lahir" => "Sragen, 5 Februari 1923",
+        "Karier" => "- Pada masa pendudukan Jepang mengikuti pendidikan militer Pembela Tanah Air (PETA) di Bogor
+                - Diangkat menjadi Shodanco Peta di Solo
+                - Setelah Proklamasi Kemerdekaan RI, masuk TKR.
+                - Ditugaskan ke Sumatera Barat pada tahun 1958 untuk menumpas pemberontakan PRRl sebagai Komandan Batalyon A Komando Operasi 17 Agustus
+                - Menjadi Kepala Staf Resimen Tim Pertempuran (RIP) II Diponegoro di Bukittinggi.",
+        "Wafat" => "1 Oktober 1965",
+        ),
+      10=> array(
+        "Foto" => "<img src = 'Karel.JPG' width ='100' height = '80'>",  
+        "Nama Pahlawan" => "Ajun Inspektur Polisi Dua (Anumerta) Karel Sadsuitubun",
+        "Lahir" => "Tual, 14 Oktober 1928",
+        "Karier" => "- Tamat dari Sekolah Polisi Negara di Ambon ia diangkat sebagai Agen Polisi Tingkat II
+                - Mendapat tugas dalam kesatuan Brigade Mobil (Brimob) di Ambon
+                - Ditempatkan pada kesatuan Brimob Dinas Kepolisian Negara di Jakarta
+                - Tahun 1955 dipindahkan ke Medan Sumatera Utara dan tahun 1958 dipindahkan ke Sulawesi..",
+        "Wafat" => "1 Oktober 1965",
+        ),
+            
+    
+    // Lanjutkan sampai 10 Nama Pahlawan
+);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style type="text/css">
+        .foto {
+            width: 100px;
+            height: 80px;
+        }
+    </style>
+</head>
+<body>
+    <!--Pemilihan tabel Nama Pahlawan-->
+<b>Nama : Wendi Saputra</b>
+</br>
+<b>NIM : 701220128</b>
+</br>
+<b> NRP %5 = 128% * 5 = 6,4 => Nama Pahlawan </b>
+    <?php
+    // Menampilkan data dalam bentuk tabel HTML
+echo "<center><b>NAMA PAHLAWAN</b></center>";
+echo "</br>";
+echo "<table border='1'>";
+echo "<tr><th>No</th><th>Foto</th><th>Nama Pahlawan</th><th>Lahir<th>Karier</th><th>Wafat</th></tr>";
+$nomor = 1;
+
+// Menggunakan loop untuk menampilkan data ke dalam tabel
+foreach ($Pahlawan as $data) {
+    echo "<tr>";
+    echo "<td>$nomor</td>";
+    echo "<td>" . $data['Foto'] . "</td>";
+    echo "<td>" . $data['Nama Pahlawan'] . "</td>";
+    echo "<td>" . $data['Lahir'] . "</td>";
+    echo "<td>" . $data['Karier'] . "</td>";
+    echo "<td>" . $data['Wafat'] . "</td>";
+    echo "</tr>";
+    $nomor++;
+}
+
+echo "</table>";
+
+
+    ?>
+
+</body>
+</html>
